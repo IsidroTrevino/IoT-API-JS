a0632da63c84e538b68f68a56210525cae6a6a78
IsidroTrevino <A00837104@tec.mx>
2023-11-20 23:21:13 -0600

    Adicion de pagina de login funcional

--- static/script.js
+++ static/script.js
@@ -1,19 +1,37 @@
-fetch('http://localhost:3000/api/getUser') 
-    .then(response => response.json())
-    .then(users => {
-        const usersBody = document.getElementById('users-body');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('login-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${user.nombreUsuario}</td>
-                <td>${user.nombre} ${user.apellido}</td>
-                <td>${user.idUsuario}</td>
-                <td>${user.contrasena}</td>
-            `;
-            usersBody.appendChild(row);
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const apiUrl = 'http://localhost:3000/api/obtenerUsuario';
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nombreUsuario: username,
+                contrasena: password
+            }),
+        })
+        .then(response => response.json())
+        .then(userData => {
+            if (userData && userData.length > 0) {
+                // Se encontró un usuario coincidente
+                window.location.href = 'Dashboard.html'; // Redirigir al dashboard
+            } else {
+                const errorMessage = 'Usuario o contraseña incorrectos.';
+                console.log(response);
+                document.getElementById('error-message').textContent = errorMessage;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const errorMessage = 'Usuario o contraseña incorrectos.';
+            document.getElementById('error-message').textContent = errorMessage;
         });
-    })
-    .catch(error => {
-        console.error('Error fetching users data:', error);
     });
+});
